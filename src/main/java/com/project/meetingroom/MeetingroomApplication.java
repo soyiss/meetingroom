63e502e962876b85fc2aaee28c5b4e96fabc343e
soyiss <zwcrvl@naver.com>
2026-08-02 12:08:47 +0900

    회의실 예약 시스템 _기본세팅완료

--- src/main/java/com/project/meetingroom/MeetingroomApplication.java
+++ src/main/java/com/project/meetingroom/MeetingroomApplication.java
@@ -1,8 +1,10 @@
 package com.project.meetingroom;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.project.meetingroom.mapper")
 @SpringBootApplication
 public class MeetingroomApplication {
 
